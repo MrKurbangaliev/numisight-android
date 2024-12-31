0303ea46c0df0d7e00887f0948d2ba2e1761001b
Danat Kurbangaliiev <kurbangalievdm@gmail.com>
2024-12-31 22:48:28 +0200

    Add Realm dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation(libs.realm.gradle.plugin)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
